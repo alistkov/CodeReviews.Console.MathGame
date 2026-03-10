5fe607d07fac826fca89b3942971fca923ab73cd
alistkov <antonlistkov@gmail.com>
2026-03-10 01:00:49 +0100

    add starting game message

--- AntonListkov.MathGame/Program.cs
+++ AntonListkov.MathGame/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Welcome to the Math game!");
+Console.WriteLine("Enter your name:");
 
-Console.WriteLine("Init project");
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("==========================");
+Console.WriteLine($"Hello, {name}! It's {date.DayOfWeek}. Let's play Math Game!");
+Console.WriteLine(@"What game would you like to play today?
+1 - Addition
+2 - Subtraction
+3 - Multiplication
+4 - Division
+0 - Exit");
+Console.WriteLine("==========================");
+
+var userSelection = int.Parse(Console.ReadLine());
+Console.WriteLine(userSelection);
